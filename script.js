66cb687cec8620588db796bda08083a10afed9a5
dencioras1 <denaskh@gmail.com>
2025-04-29 12:55:01 +0200

    Added criteria, as well as an ambitious plan.

---
+++ script.js
@@ -0,0 +1,18 @@
+// Calculator should be able to handle:
+// 1. Basic operations (+, -, *, /)
+// 2. ONLY pairs of numbers (a and b)
+//      If there are more than 2 numbers inputted (a + b + c), evaluate a and b, and then proceed to c
+// 3. Should allow user to clear calculator input/memory
+// 4. Display should update with each input
+// 5. Avoid unexpected errors:
+//      Division by zero
+//      Operator followed by an operator (Eg. a + - b)
+//      Multiple decimal points (Eg. 1.1.1)
+//      Pressing = before full input is provided
+// 5. Entering a new digit after an evaluation should clear the memory/input
+
+// Extra credit:
+// 1. sqrt, pow, x^y, fact
+// 2. Decimals
+// 3. Backspace
+// 4. Keyboard support
